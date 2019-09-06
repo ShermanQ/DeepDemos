0f019904423fb8aa2dc29424d6a0d0b78f2f0133
ShermanQ <qiuwentao0823@gmail.com>
2019-09-06 10:29:47 +0800

    test file of py

---
+++ conv-demo/pretensor/pre.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+x = tf.placeholder(tf.float32)
+W = tf.Variable(1.0)
+b = tf.Variable(2.0)
+y = W * x + b
+with tf.Session as sess:
+    tf.global_variables_initializer().run()
+    # fetch = y.eval(feed_dict={x:3.0})
+    print(sess.run(tf.get_variable("w")))
